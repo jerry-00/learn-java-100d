b682c5b2e5cc41af47960e8c885332516bf7e0a7
jerry <jerry_09@aliyun.com>
2022-11-25 00:22:43 +0800

    T1

--- java-collection/src/main/java/Test.java
+++
@@ -1,6 +0,0 @@
-package PACKAGE_NAME;/**
-@author Jerry
-@create 2022-11-24-23:52
-*/
-public class Test {
-}
---
+++ java-collection/src/main/java/main/Test.java
@@ -0,0 +1,9 @@
+package main;/**
+@author Jerry
+@create 2022-11-24-23:52
+*/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
